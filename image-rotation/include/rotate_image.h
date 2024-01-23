4858d620653346954dfd1e177b5d07a0ad048361
SergeyUvarov <50031654+SergeyUvarov@users.noreply.github.com>
2024-01-24 00:33:36 +0300

    Delete image-rotation directory

--- image-rotation/include/rotate_image.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROT_IMG_H
-#define ROT_IMG_H
-
-#include "image.h"
-
-/* создаёт копию изображения, которая повёрнута на 90 градусов */
-struct image rotate(struct image const source);
-
-#endif
